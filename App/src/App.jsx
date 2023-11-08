446f819397c2948cc82b29107a213d1eec7da02b
Anas Ben Messaoud <anasbenmessaoud2000@gmail.com>
2023-11-08 17:51:09 +0100

    far away

--- App/src/App.jsx
+++ App/src/App.jsx
@@ -1,27 +1,8 @@
 //imports
 import { useState } from 'react';
-import './App.css';
 //var
 
 //exports
 export default function App() {
-    const [step, addStep] = useState(1);
-    const [count, addCount] = useState(0);
-    let date = new Date('june 21 2027')
-    date.setDate(date.getDate + count)
-    return(
-        <>
-        <div className='stepCounter'>
-            <button>+</button>
-            <h2>Step:{step}</h2>
-            <button>-</button>
-        </div>
-        <div className='dateCounter'>
-            <button>+</button>
-            <h2>Count:{count}</h2>
-            <button>-</button>
-        </div>
-        <h2>{String(date)}</h2>
-        </>
-    )
+    
 }
