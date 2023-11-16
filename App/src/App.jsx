175b530dfd68aea098e1ac9e9e0f2141ff944d95
Anas Ben Messaoud <anasbenmessaoud2000@gmail.com>
2023-11-16 20:35:18 +0100

    app

--- App/src/App.jsx
+++ App/src/App.jsx
@@ -28,7 +28,7 @@ const PageNotFound = lazy(() => import("./pages/PageNotFound"));
 // dist/assets/index-59fcab9b.css   30.56 kB │ gzip:   5.14 kB
 // dist/assets/index-f7c12d89.js   572.44 kB │ gzip: 151.29 kB
 
-function App() {
+export default function App() {
   return (
     <AuthProvider>
       <CitiesProvider>
@@ -61,5 +61,3 @@ function App() {
     </AuthProvider>
   );
 }
-
-export default App;
